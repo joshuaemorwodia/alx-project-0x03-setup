24c2c5dc4d239ce82bb64a43f81358c74841adee
Joshua Emorwodia <joshuaemorwodia@gmail.com>
2025-06-10 18:36:55 +0100

    Completed layout components

---
+++ components/layouts/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/layouts/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <div className="pt-32 text-center">
+        <h1 className="text-4xl font-bold text-gray-800">Welcome to Splash App!</h1>
+      </div>
+    </Layout>
+  );
+}
